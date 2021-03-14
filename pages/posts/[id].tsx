0c4687e86195fa130e5d73ebc07a6035c7b31f9e
max <harumaxy@gmail.com>
2021-03-14 18:48:00 +0900

    fix page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,25 +1,29 @@
 import Layout from "../../components/layout";
-import { getAllPostIds, getPostData } from "../../lib/posts";
+import { getAllPostIds, getPostData, PostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import React from "react";
 
-export default function Post({ postData }) {
+const Post: React.FC<{
+  postData: PostData;
+}> = ({ postData }) => {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
+        <div className="pb-6">
+          <h1 className="text-3xl font-extrabold">{postData.title}</h1>
           <Date dateString={postData.date} />
         </div>
+
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
   );
-}
+};
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -37,3 +41,5 @@ export async function getStaticProps({ params }) {
     },
   };
 }
+
+export default Post;
