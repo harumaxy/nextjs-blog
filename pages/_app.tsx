b0c6577cb64b5b86836d5e8c0cfa21550cf5760a
max <harumaxy@gmail.com>
2021-03-14 22:37:59 +0900

    highlight all prism

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import { useEffect } from "react";
+import { highlightAll } from "prismjs";
 import { AppPropsType } from "next/dist/next-server/lib/utils";
 import Layout from "../components/layout";
 import "../styles/global.css";
 
 const App: React.FC<AppPropsType> = ({ Component, pageProps, router }) => {
+  useEffect(() => {
+    highlightAll();
+  }, []);
   return (
     <Layout home={router.route === "/"}>
       <Component {...pageProps} />
